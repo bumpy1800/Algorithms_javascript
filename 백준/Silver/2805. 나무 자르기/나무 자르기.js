2ccf12e0082f57bb2fa77eac4c8b08d3f3b28c8f
bumpy1800 <51107943+bumpy1800@users.noreply.github.com>
2024-03-17 20:09:24 +0900

    [Silver II] Title: 나무 자르기, Time: 1308 ms, Memory: 171280 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.js
@@ -0,0 +1,34 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.split(" ").map(Number));
+
+const goalHeight = input.shift()[1];
+
+let treeArray = input.flatMap((e)=>e).sort((a,b)=>a-b)
+let head = treeArray[treeArray.length-1]
+let tail = 0
+let maxHeight = 0
+
+while(tail <= head){
+  let mid = Math.floor((tail + head)/2)
+  let sum = 0  
+
+  treeArray.forEach((el)=>{
+    if((el - mid) > 0){
+      sum += el - mid
+    }
+  })
+  if(sum >= goalHeight){
+    if(mid > maxHeight){
+      maxHeight = mid
+    }
+    tail = mid+1
+  }else if(sum < goalHeight){
+    head = mid-1
+  }
+}
+
+console.log(maxHeight)
